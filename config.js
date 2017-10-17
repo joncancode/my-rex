49ceca6a56cbedd36d905d95cacfd52b4b239da8
joncancode <jc.haines19@gmail.com>
2017-10-16 22:45:30 -0400

    connecting mongo and heroku

--- config.js
+++ config.js
@@ -2,8 +2,8 @@ require('dotenv').config();
 
 exports.DATABASE_URL = process.env.DATABASE_URL ||
                        global.DATABASE_URL ||
-                      'mongodb://localhost:27017/my-rex';
+                      'mongodb://localhost/8080';
 exports.TEST_DATABASE_URL = (
   process.env.TEST_DATABASE_URL ||
- 'mongodb://localhost:27017/test-my-rex');
+ 'mongodb://localhost/8080');
 exports.PORT = process.env.PORT || 8080;
