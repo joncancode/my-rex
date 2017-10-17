ef7bd240bea2f8e399855a85d778ddf1e77db22b
joncancode <jc.haines19@gmail.com>
2017-10-17 11:25:15 -0400

    changed ip to local host and updated app name in travis yml file

--- config.js
+++ config.js
@@ -2,8 +2,8 @@ require('dotenv').config();
 
 exports.DATABASE_URL = process.env.DATABASE_URL ||
                        global.DATABASE_URL ||
-                      'mongodb://127.0.0.1/8080';
+                      'mongodb://localhost/8080';
 exports.TEST_DATABASE_URL = (
   process.env.TEST_DATABASE_URL ||
- 'mongodb://127.0.0.1/8080');
+ 'mongodb://localhost/8080');
 exports.PORT = process.env.PORT || 8080;
